52319fd985091e5e4c6e6d20aa01e525ced4e075
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-07-11 08:20:15 +0900

    feat: long poling chat

---
+++ src/main/java/com/example/realtimecommunication/domain/chat/presentation/controller/LongPolingChatController.java
@@ -0,0 +1,33 @@
+package com.example.realtimecommunication.domain.chat.presentation.controller;
+
+import com.example.realtimecommunication.domain.chat.domain.ChatMessage;
+import com.example.realtimecommunication.domain.chat.presentation.dto.ChatMessageDto;
+import com.example.realtimecommunication.domain.chat.service.LongPolingChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+public class LongPolingChatController {
+    private final LongPolingChatService longPolingChatService;
+
+    @GetMapping("/long-poling-subscribe")
+    public DeferredResult<List<ChatMessageDto>> subscribe(
+            @RequestParam String roomId,
+            @RequestParam(defaultValue = "0") Long lastMessageId
+    ){
+        return longPolingChatService.waitForNewMessages(roomId, lastMessageId);
+    }
+
+    @PostMapping("/long-poling-send")
+    public ResponseEntity<Void> sendMessage(@RequestBody ChatMessage message){
+        longPolingChatService.saveMessage(message);
+        return ResponseEntity.ok().build();
+    }
+
+}
